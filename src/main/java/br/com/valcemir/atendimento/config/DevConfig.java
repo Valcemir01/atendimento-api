ceea80c60c8909d8eb1f45321b5d176f95119a18
valcemir01 <valcemirads@gmail.com>
2021-08-19 23:04:05 -0300

    Criação do perfil de dev

---
+++ src/main/java/br/com/valcemir/atendimento/config/DevConfig.java
@@ -0,0 +1,27 @@
+package br.com.valcemir.atendimento.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.valcemir.atendimento.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService service;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean InstanciaDB() {
+		if(strategy.equals("create")) {
+			this.service.IniciarDB();
+		}		
+		return false;
+	}
+}
